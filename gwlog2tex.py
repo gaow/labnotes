06c0beac4a638b910c8bdff1e8328361e9efd1b2
gaow <gaow@e741e9a7-cddc-45ea-a921-d623c1f69cea>
2012-05-15 07:34:14 +0000

    completed gw_log

--- gwlog2tex.py
+++ gwlog2tex.py
@@ -1,158 +1,224 @@
-import sys
-from subprocess import call
+import sys, re, os
 
-def getDocHead(args):
-    if args.title != None:
-        mytitle = list(args.title)
-        mytitle = [w.replace('$', '\$') for w in mytitle]
-        mytitle = [w.replace('_', '\_') for w in mytitle]
-        mytitle = [w.replace('%', '\%') for w in mytitle]
-        mytitle = [w.replace('&', '\&') for w in mytitle]
-        mytitle = ''.join(mytitle)
-    else:
-        mytitle = args.title
-    if args.author != None:
-        myauthor = list(args.author)
-        myauthor = [w.replace('$', '\$') for w in myauthor]
-        myauthor = [w.replace('_', '\_') for w in myauthor]
-        myauthor = [w.replace('%', '\%') for w in myauthor]
-        myauthor = [w.replace('&', '\&') for w in myauthor]
-        myauthor = ''.join(myauthor)
-    else:
-        myauthor = args.author
-    docHead = ['\\documentclass[oneside, 10pt]{article}\n', '\\usepackage{geometry}\n', '\\usepackage{fullpage}\n', '\\usepackage{amsmath}\n', '\\usepackage{booktabs}\n', '\\usepackage{listings}\n', '\\usepackage{subfig}\n', '\\usepackage{amssymb}\n', '\\usepackage{amsthm}\n', '\\usepackage{bm}\n', '\\usepackage{extarrows}\n', '\\usepackage{fancyhdr}\n', '\\usepackage{fancyvrb}\n', '\\usepackage[pdftex]{graphicx}\n', '\\usepackage[pdfstartview=FitH]{hyperref}\n', '\\usepackage{ctable}\n', '\\usepackage[dvipsnames]{xcolor}\n', '\\usepackage{verbatim}\n', '\\usepackage{minted}\n', '\\usepackage{pdfpages}\n', '\\usepackage{titlesec}\n','\\renewcommand\\rmdefault{bch}\n', '\\newcommand{\\ie}{\\textit{i.e.}}\n', '\\newcommand\\me{\\mathrm{e}}\n', '\\newcommand\\mlog{\\mathrm{log}}\n', '\\linespread{1.1}\n', '\\setlength{\\parskip}{8pt plus1pt minus2pt}\n', '\\parindent 0ex\n', '\\geometry{left=0.8in,right=0.8in,top=0.8in,bottom=0.8in}', '\\titleformat{\\subsubsection}\n', '{\\color{MidnightBlue}\\normalfont\\large\\bfseries}\n', '{\\color{MidnightBlue}\\thesection}{1em}{}\n', '\\definecolor{bg}{rgb}{0.95,0.95,0.95}\n', '\\title{%s}\n' % mytitle, '\\author{%s}\n' % myauthor, '\\date{Compiled: \\today}\n', '\\raggedbottom\n', '\\begin{document}\n\n']
-    return docHead
+def recodeKw(line):
+    if not line:
+        return None
+    for item in [('$', '\$'),('{', '\{'),('}', '\}'),('%', '\%'),
+            ('_', '\_'),('&', '\&'),('<', '$<$'),('>', '$>$')]:
+        line = line.replace(item[0], item[1])
+    if not line.startswith('#'):
+        line.replace('#', '\#')
+    return line
 
-def recodeLatexKw(tmpline):
-    tmpline = list(tmpline)
-    tmpline = [w.replace('$', '\$') for w in tmpline]
-    tmpline = [w.replace('{', '\{') for w in tmpline]
-    tmpline = [w.replace('}', '\}') for w in tmpline]
-    tmpline = [w.replace('%', '\%') for w in tmpline]
-    tmpline = [w.replace('_', '\_') for w in tmpline]
-    tmpline = [w.replace('&', '\&') for w in tmpline]
-    tmpline = [w.replace('<', '$<$') for w in tmpline]
-    tmpline = [w.replace('>', '$>$') for w in tmpline]
-    if tmpline[0] != '#':
-        tmpline = [w.replace('#', '\#') for w in tmpline]
-    else:
-        #FIXME
-        pass 
-    return ''.join(tmpline)
-                
-def getDocBody(args, extension):
-    myDoc = getDocHead(args)
-    if args.title != None:
-        myDoc.append('\\maketitle\n')
-    if extension == 'history':
+def wraptxt(line, sep, by):
+    sline = ''
+    i = 0
+    for item in list(line):
+        if item == '\n':
+            i = 0
+        if i % by  == 0 and i > by:
+            sline += item + sep + '\n'
+        else:
+            sline += item
+        i += 1
+    return sline
+
+class LogToTex:
+    def __init__(self, args):
+        self.title = recodeKw(args.title)
+        self.author = recodeKw(args.author)
+        self.text = []
         for fn in args.filename:
             try:
-                open('.'.join([fn, extension]))
+                self.text.extend(list(line for line in (l.strip() for l in open(fn).readlines()) if line))
             except IOError as e:
-                print("***{0}***".format(e))
-                sys.exit(1)
-            myDoc.append('\\inputminted[linenos, numberblanklines=true, fontsize=\\footnotesize]{bash}{%s}\n' % (fn + '.' + extension))
-    else:
-        for fn in args.filename:
-            try:
-                fob = open('.'.join([fn, extension]), 'r')
-            except IOError as e:
-                print("***{0}***".format(e))
-                sys.exit(1)
-            mylines =  fob.readlines()
-            fob.close()
-            
-            index = 0
-            while index < len(mylines):
-                # Program source codes in this line
-                if mylines[index].startswith('$'):
-                    tmpline = ''.join(['\\mint[bgcolor=bg, numberblanklines=true, fontsize=\\footnotesize]{bash}|', mylines[index][1:-1], '|\n'])
-                    if not (index == 0 or mylines[index-1].startswith('$')):
-                        tmpline = ''.join(['\n', tmpline])
-                    if not (index == len(mylines) - 1 or mylines[index+1].startswith('$')):
-                        myDoc.append(''.join([tmpline, '\n']))
-                    else:
-                        myDoc.append(tmpline)
-                    index += 1
+                sys.exit(e)
+        self.blocks = {}
+        for item in ['err', 'out', 'bash', 'list']:
+            self.blocks[item] = []
+#        for idx, item in enumerate(self.text):
+#            print idx, item
+        self.m_parseBlocks()
+#        print(self.blocks)
+#        for idx, item in enumerate(self.text):
+#            print idx, item
+        self.m_blockizeBash()
+        self.m_blockizeOut()
+        self.m_blockizeList()
+        self.m_parseText()
+        #print('\n'.join(self.text))
+        #sys.exit(0)
 
-                elif mylines[index].startswith('>>>'):
-                    tmpline = ''.join(['\\mint[bgcolor=bg, numberblanklines=true, fontsize=\\footnotesize]{python}|', mylines[index][3:-1], '|\n'])
-                    if not (index == 0 or mylines[index-1].startswith('>>>')):
-                        tmpline = ''.join(['\n', tmpline])
-                    if not (index == len(mylines) - 1 or mylines[index+1].startswith('>>>')):
-                        myDoc.append(''.join([tmpline, '\n']))
-                    else:
-                        myDoc.append(tmpline)
-                    index += 1
-                    
-                # Itemizations in this line (and its following lines)
-                elif mylines[index].startswith('---'):
-                    endIndex = None
-                    try:
-                        for i in range(index+1, len(mylines)):
-                            if mylines[i].startswith('---'):
-                                endIndex = i
-                                break
-                        if endIndex is None:
-                            raise ValueError('symbol \'---\' must appear in pairs')
-                    except ValueError as v:
-                        print('***{0}***'.format(v))
-                        sys.exit(1)
-                    else:
-                        myDoc.append('\\begin{itemize}\n')
-                        hasItem = False
-                        for i in range(index+1, endIndex):
-                            if mylines[i].startswith('-'):
-                                hasItem = True
-                                mylines[i] = recodeLatexKw(mylines[i])
-                                myDoc.append(' '.join(['\\item', mylines[i][1:]]))
-                            else:
-                                mylines[i] = recodeLatexKw(mylines[i])
-                                myDoc.append(mylines[i])
-                        if hasItem == False:
-                            print('ERROR: missing \\item entries between lines {0} and {1}'.format(index, endIndex))
-                            sys.exit(1) 
-                        myDoc.append('\\end{itemize}\n')
-                        index = endIndex + 1
-                
-                # section symbols in this line
-                elif mylines[index].startswith('###') and mylines[index+1].startswith('#') and mylines[index+2].startswith('###'):
-                    mylines[index+1] = recodeLatexKw(mylines[index+1])
-                    myDoc.append(''.join(['\\section{', mylines[index+1][1:-1], '}\n']))
-                    index += 3
-                    
-                elif mylines[index].startswith('#!-'):
-                    mylines[index] = recodeLatexKw(mylines[index])
-                    myDoc.append(''.join(['\\subsubsection*{', mylines[index][3:-1], '}\n']))
-                    index += 1
-                    
-                elif mylines[index].startswith('#!'):
-                    mylines[index] = recodeLatexKw(mylines[index])
-                    myDoc.append(''.join(['\\subsection{', mylines[index][2:-1], '}\n']))
-                    index += 1
-                
-                # Nothing special, just a plain line here   
-                else:
-                    if mylines[index] == '\n':
-                        myDoc.append('\n')
-                    else:
-                        myDoc.append(recodeLatexKw(mylines[index])[:-1] + '\\\\' + '\n')
-                    index += 1
-                    
-    myDoc.append('\\end{document}')
-    tmpFilename = '-'.join(args.filename) + '_' + extension
-    outFilename = tmpFilename + '.tex'
-    fob = open(outFilename, 'w')
-    fob.writelines(myDoc)
-    fob.close()
-    # Run LaTeX
-    call(["pdflatex", "-shell-escape", outFilename])
-    call(["pdflatex", "-shell-escape", outFilename])
-    call(["rm", tmpFilename + '.aux'])
-    call(["rm", tmpFilename + '.log'])
-    call(["rm", tmpFilename + '.out'])
-    try:
-        open(tmpFilename + '.pyg')
-    except IOError as e:
-        sys.exit(1)
-    call(["rm", tmpFilename + '.pyg'])
+    def get(self, code):
+        if code and len(self.blocks['err']) > 0:
+            for idx in range(len(self.text)):
+                for item in self.blocks['err']:
+                    if idx in range(item[0], item[1]+1):
+                        self.text[idx] = ''
+                        break
+        self.text = [x for x in self.text if len(x)>0]
+        return '''\\documentclass[oneside, 10pt]{article}
+\\usepackage{geometry}
+\\usepackage{fullpage}
+\\usepackage{amsmath}
+\\usepackage{booktabs}
+\\usepackage{listings}
+\\usepackage{amssymb}
+\\usepackage{amsthm}
+\\usepackage{bm}
+\\usepackage{extarrows}
+\\usepackage{fancyhdr}
+\\usepackage{fancyvrb}
+\\usepackage[pdftex]{graphicx}
+\\usepackage[pdfstartview=FitH]{hyperref}
+\\usepackage[dvipsnames]{xcolor}
+\\usepackage{minted}
+\\usepackage{titlesec}
+\\renewcommand\\rmdefault{bch}
+\\newcommand{\\ie}{\\textit{i.e.}}
+\\newcommand\\me{\\mathrm{e}}
+\\newcommand\\mlog{\\mathrm{log}}
+\\linespread{1.1}
+\\setlength{\\parskip}{8pt plus1pt minus2pt}
+\\parindent 0ex
+\\geometry{left=0.8in,right=0.8in,top=0.8in,bottom=0.8in}
+\\titleformat{\\subsubsection}
+{\\color{MidnightBlue}\\normalfont\\large\\bfseries}
+{\\color{MidnightBlue}\\thesection}{1em}{}
+\\definecolor{bg}{rgb}{0.95,0.95,0.95}
+\\title{%s}
+\\author{%s}
+\\date{Last updated: \\today}
+\\raggedbottom
+\\begin{document}
+        %s\n%s
+\\end{document}
+		''' % (str(self.title), str(self.author), '\\maketitle' if self.title else '', '\n'.join(self.text))
+
+    def m_parseBlocks(self):
+        idx = 0
+        while True:
+            if idx >= len(self.text):
+                break
+            if self.text[idx].startswith('#{') and '--' not in self.text[idx]:
+                # define block
+                bname = re.sub(r'\s*', '', self.text[idx].split('{')[1])
+                endidx = None
+                self.text[idx] = ''
+                # find end of block
+                for i in range(idx+1, len(self.text)):
+                    if self.text[i].startswith('#}') and bname in self.text[i]:
+                        endidx = i
+                        break
+                if not endidx:
+                    sys.exit('ERROR: #{ %s and #} %s must appear in pairs' %s (bname))
+                # combine block values
+                for i in range(idx + 1, endidx):
+                    self.text[idx] += self.text[i] + '\n'
+                del self.text[(idx + 1) : (endidx + 1)]
+                    # keep block index
+                self.blocks[bname].append(idx)
+            idx += 1
+            continue
+        #
+        for idx, item in enumerate(self.text):
+            # define err block
+            if item.startswith('#{') and '--' in item:
+                endidx = None
+                for i in range(idx+1, len(self.text)):
+                    if self.text[i].startswith('#}') and '--' in self.text[i]:
+                        endidx = i
+                        break
+                if not endidx:
+                    sys.exit('ERROR: comment blocks must appear in pairs')
+                self.blocks['err'].append([idx, endidx])
+                self.text[idx] = ''
+                self.text[endidx] = ''
+        return
+
+    def m_blockizeBash(self):
+        if len(self.blocks['bash']) == 0:
+            return
+        for i in self.blocks['bash']:
+           self.text[i] = '''
+\\begin{Verbatim}[samepage=false, fontfamily=courier,
+fontsize=\\tiny, formatcom=\\color{black},
+frame=lines, framerule=1pt, framesep=2mm,
+label=\\fbox{BASH}, labelposition=topline]\n%s
+\\end{Verbatim}
+           ''' % wraptxt(self.text[i], '\\', 200)
+        return
+
+    def m_blockizeOut(self):
+        if len(self.blocks['out']) == 0:
+            return
+        for i in self.blocks['out']:
+           self.text[i] = '''
+\\begin{Verbatim}[samepage=false, fontfamily=courier,
+fontsize=\\footnotesize, formatcom=\\color{blue},
+frame=lines, framerule=1pt, framesep=2mm,
+label=\\fbox{OUTPUT}, labelposition=topline]\n%s
+\\end{Verbatim}
+           ''' % wraptxt(self.text[i], '', 100)
+        return
+
+    def m_blockizeList(self):
+        if len(self.blocks['list']) == 0:
+            return
+        for i in self.blocks['list']:
+           self.text[i] = '\\begin{itemize}%s\n\\end{itemize}' % recodeKw(re.sub(r'^#|\n#', '\\item ', self.text[i]))
+        return
+
+    def m_parseText(self):
+        skip = []
+        for item in self.blocks.keys():
+            for i in self.blocks[item]:
+                try:
+                    skip.extend(i)
+                except:
+                    skip.append(i)
+        idx = 0
+        while idx < len(self.text):
+            if idx in skip:
+                # already processed
+                idx += 1
+                continue
+            if self.text[idx] == '':
+                idx += 1
+                continue
+            if '####' in self.text[idx]:
+                # too many #'s
+                sys.exit("You have so many urgly '#' symbols in a regular line. Please modify this line '{0}'".format(self.text[idx]))
+            if not self.text[idx].startswith('#'):
+                # regular cmd
+                self.text[idx] = ''.join(['\\mint[bgcolor=bg, numberblanklines=true, fontsize=\\footnotesize]{bash}|' + x + '|' for x in wraptxt(self.text[idx], '\n', 55).split('\n') if x != ''])
+                idx += 1
+                continue
+            if self.text[idx].startswith('###') and self.text[idx+1].startswith('#') and self.text[idx+2].startswith('###'):
+                # section
+                self.text[idx] = ''
+                self.text[idx + 1] = '\\section{' + recodeKw(self.text[idx + 1][1:]) + '}'
+                self.text[idx + 2] = ''
+                idx += 3
+                continue
+            if self.text[idx].startswith('#!!'):
+                # subsection, subsubsection ...
+                self.text[idx] = '\\subsubsection*{' + recodeKw(self.text[idx][3:]) + '}'
+                idx += 1
+                continue
+            if self.text[idx].startswith('#!'):
+                # subsection, subsubsection ...
+                self.text[idx] = '\\subsection{' + recodeKw(self.text[idx][2:]) + '}'
+                idx += 1
+                continue
+            if self.text[idx].startswith('#*'):
+                # date
+                self.text[idx] = '\\fbox{' + self.text[idx][2:] + '}'
+                idx += 1
+                continue
+            if self.text[idx].startswith('#'):
+                # a plain line here
+                self.text[idx] = '\n' + recodeKw(self.text[idx][1:]) + '\n'
+                idx += 1
+                continue
+        return
