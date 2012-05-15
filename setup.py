7e4321701e63b8b22d025abb7d0a65f0de070853
gaow <gaow@e741e9a7-cddc-45ea-a921-d623c1f69cea>
2012-05-15 08:14:06 +0000

    add python version check

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
 try:
    from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
    from distutils.command.build_py import build_py
-
 import sys, os
+try:
+    import argparse
+except ImportError:
+    sys.exit('The program requires Python 2.7.2 or higher, or Python 3.2 or higher. Please upgrade your version (%s) of Python and try again.' % (sys.version.split()[0]))
+#
 setup(name = 'gw_log',
     version = '1.0',
     py_modules = [
